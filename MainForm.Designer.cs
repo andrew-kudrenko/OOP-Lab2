e28562a2a91af628f99c9006f191be1ece202293
Andrew Kudrenko <andrewkudrenko19@gmail.com>
2022-02-07 20:34:07 +0300

    Started a WinForms project. Created tables for storing groups and students. Filled by mock data.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,18 @@
+﻿
+namespace StudentsManager
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
